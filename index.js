e829dd7be13560bcc756cce539fabfcd62093287
Faith <thao.faith2@gmail.com>
2021-07-09 18:14:58 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = 
+document.createElement('H1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'FAITH is the champion';
